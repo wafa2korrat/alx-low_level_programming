b9182a38d077489d5f946fa02fa0ff19a544254d
wafa2korrat <wafa2ahmedkorrat@gmail.com>
2023-06-16 12:23:25 -0700

    Edit 0-positive_or_negative.c file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,13 +17,13 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if ("n = 0")
-	{
-		printf("%d is zero\n", n);
-	}
 	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
 	}
 
 	return (0);
