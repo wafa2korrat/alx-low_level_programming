a0e987bd80fac9544a2b831b20bdecaf7af7e377
wafa2korrat <wafa2ahmedkorrat@gmail.com>
2023-06-16 13:43:17 -0700

    Create 4-print_alphabt.c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		if (ch != 101 && ch != 113)
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
