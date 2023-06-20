c2f80dc7adf9ef8a9f7a69188f20b7684359a506
wafa2korrat <wafa2ahmedkorrat@gmail.com>
2023-06-20 16:32:31 -0700

    Edit 9-times_table.c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,41 +7,26 @@ void times_table(void)
 {
 	int row;
 	int num;
-	int rez;
+	int res;
 
 	for (row = 0; row < 10; row++)
 	{
-		for (num = 0; num <= 9; num++)
+		for (num = 0; num < 10; num++)
 		{
-			rez = num * row;
-			if (num == 0)
-			{
-				_putchar(rez + 48);
-				_putchar(',');
+			res = num * row;
+			if (num > 0 && num < 9)
 				_putchar(' ');
-			}
+			if (res <= 9)
+				_putchar(res + 48);
 			else
 			{
-				if (rez <= 9)
-				{
-					_putchar(' ');
-					_putchar(rez + 48);
-					if (num < 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
-				else
-				{
-					_putchar((rez / 10) + 48);
-					_putchar((rez % 10) + 48);
-					if (num < 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
+				_putchar((res / 10) + 48);
+				_putchar((res % 10) + 48);
+			}
+			if (num < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
