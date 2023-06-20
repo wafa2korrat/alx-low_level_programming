9063ac7c314aff2d8e19f32c9a25d9b7d120d245
wafa2korrat <wafa2ahmedkorrat@gmail.com>
2023-06-20 11:03:06 -0700

    Create 2-print_alphabet_x10.c file and Edit main.h file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the lowercase alphabet to stdout
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int ch;
+	int row_num = 0;
+
+	for (row_num = 0; row_num <= 10; row_num++)
+	{
+	for (ch = 97; ch <= 122; ch++)
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
