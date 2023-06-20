4c28c7e963946b1518bcb74f0dbe3cd2acdf7a02
wafa2korrat <wafa2ahmedkorrat@gmail.com>
2023-06-20 16:11:35 -0700

    Edit 9-times_table.c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,30 +14,33 @@ void times_table(void)
 		for (num = 0; num <= 9; num++)
 		{
 			rez = num * row;
-			if (rez == 0)
+			if (num == 0)
 			{
 				_putchar(rez + 48);
 				_putchar(',');
 				_putchar(' ');
 			}
-			else if (rez <= 9)
-			{
-				_putchar(' ');
-				_putchar(rez + 48);
-				if (num < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
 			else
 			{
-				_putchar((rez / 10) + 48);
-				_putchar((rez % 10) + 48);
-				if (num < 9)
+				if (rez <= 9)
 				{
-					_putchar(',');
 					_putchar(' ');
+					_putchar(rez + 48);
+					if (num < 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+				else
+				{
+					_putchar((rez / 10) + 48);
+					_putchar((rez % 10) + 48);
+					if (num < 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
 				}
 			}
 		}
