940de05b34f030e3cfcf995a7e30ea0622aa95a3
wafa2korrat <wafa2ahmedkorrat@gmail.com>
2023-06-20 17:19:32 -0700

    Create 10-add.c file and Edit main.h file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @a: The first integer to add.
+ * @b: The second integer to add.
+ *
+ * Return: the result of addition.
+ */
+int add(int a, int b)
+{
+	int res = a + b;
+
+	return (res);
+}
