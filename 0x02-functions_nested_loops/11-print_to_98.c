5c16be8f814e3264fb2178bf49ceb0156f73cbb6
wafa2korrat <wafa2ahmedkorrat@gmail.com>
2023-06-20 17:40:43 -0700

    Edit 11-print_to_98.c file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,13 @@ void print_to_98(int n)
 	for (; n <= 98; n++)
 	{
 		printf("%d", n);
-		if (n < 98)
+		if (n != 98)
+			printf(", ");
+	}
+	for (; n >= 98; n--)
+	{
+		printf("%d", n);
+		if (n != 98)
 			printf(", ");
 	}
 	printf("\n");
