275bff5eff9bc6c8d338e3a8274cb06f1109cff2
wafa2korrat <wafa2ahmedkorrat@gmail.com>
2023-06-22 14:06:22 -0700

    Edit 9-fizz_buzz.c file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,7 +16,7 @@ int main(void)
 		else if ((n % 5 == 0) && (n % 3 != 0))
 			printf("Buzz ");
 		else if ((n % 3 == 0) && (n % 5 == 0))
-			printf("Fizz Buzz ");
+			printf("FizzBuzz ");
 		else
 			printf("%d ", n);
 	}
