bea77a3a47952df99980a27ce4d2f1b63a01890d
wafa2korrat <wafa2ahmedkorrat@gmail.com>
2023-06-22 13:45:42 -0700

    Create 8-print_square.c and Edit main.h files

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line.
+ * @size: the size of the square
+ *
+ */
+void print_square(int size)
+{
+	int length, width;
+	width = size;
+	length = size;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (; length > 0; length--)
+		{
+			width = size;
+			for(; width > 0; width--)
+				_putchar(35);
+			_putchar('\n');
+		}
+	}
+}
