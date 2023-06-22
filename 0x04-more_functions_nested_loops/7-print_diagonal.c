bb66b786876d3f7156d98a25b036efa1d058accc
wafa2korrat <wafa2ahmedkorrat@gmail.com>
2023-06-22 13:05:36 -0700

    Edit 7-print_diagonal.c file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,17 +8,22 @@
 void print_diagonal(int n)
 {
 	int spc, rows;
+
 	rows = n;
 
-	for (; n > 0; n--)
+	for (; n >= 0; n--)
 	{
-		_putchar(92);
-		_putchar('\n');
-		if (n != 1)
+		if (n == 0)
+			_putchar('\n');
+		else
+		{
+			_putchar(92);
+			_putchar('\n');
+		}
+		if (n > 1)
 		{
 			for (spc = (n - 1); spc < rows; spc++)
 				_putchar(32);
 		}
 	}
-	_putchar('\n');
 }
