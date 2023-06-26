8bfa6affe877fdae932ee313f0a9986b2cf163e6
wafa2korrat <wafa2ahmedkorrat@gmail.com>
2023-06-26 15:54:31 -0700

    Create 4-print_rev.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: The string to print
+ *
+ */
+void print_rev(char *s)
+{
+	char first = *s;
+
+	while (*s != '\0')
+		s++;
+	for (; *s >= first; s--)
+		_putchar(*s);
+}
