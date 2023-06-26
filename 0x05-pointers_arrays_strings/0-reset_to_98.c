6a72308adba4be3bdaf81790ce890c195e37a8f7
wafa2korrat <wafa2ahmedkorrat@gmail.com>
2023-06-26 13:23:16 -0700

    Edit 0-reset_to_98.c file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98.
+ * reset_to_98 - takes a pointer to an int and updates its value to 98.
  * @*n: the pointer to the int that will update its value to 98.
  *
  */
