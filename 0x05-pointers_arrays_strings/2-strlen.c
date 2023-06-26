c45e21efeb71170c21f7f23a7602eab87e71d8bc
wafa2korrat <wafa2ahmedkorrat@gmail.com>
2023-06-26 15:02:05 -0700

    Create 2-strlen.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: The string to be measured
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	char array_s = *s;
+	int length = sizeof(array_s);
+
+	return (length);
+}
