10a4e44bca695f956cd0a432b8c77cd5c5691793
wafa2korrat <wafa2ahmedkorrat@gmail.com>
2023-06-26 16:19:03 -0700

    Create 5-rev_string.c file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: The string to be reverse.
+ *
+ */
+void rev_string(char *s)
+{
+	char first = *s;
+
+	if (*s != '\0')
+	{
+		while (*s != '\0')
+			s++;
+		s--;
+		while (*s != first)
+		{
+			_putchar(*s);
+			s++;
+		}
+		_putchar(first);
+	}
+}
