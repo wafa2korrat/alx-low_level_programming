6b7b65507dd2c3f9e81c26d03f5c4a11be14d8e7
wafa2korrat <wafa2ahmedkorrat@gmail.com>
2023-06-26 18:33:08 -0700

    Edit 6-puts2.c file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,14 @@
  */
 void puts2(char *str)
 {
+	int i = 0;
+
 	if (*str != '\0')
 	{
-		while (*str != '\0')
+		while (str[i] != '\0')
 		{
 			_putchar(*str);
+			i = i + 2;
 			str = str + 2;
 		}
 	}
